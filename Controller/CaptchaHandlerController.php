74459ee83e5ad49d9a5d893e7421f9b8dff9903a
botdetect.support <botdetect.support@captcha.com>
2018-03-18 14:15:22 +0700

    update version to 4.2.1 and fixbugs

--- Controller/CaptchaHandlerController.php
+++ Controller/CaptchaHandlerController.php
@@ -29,6 +29,7 @@ class CaptchaHandlerController extends Controller
             \BDC_HttpHelper::BadRequest('command');
         }
 
+        $commandString = \BDC_StringHelper::Normalize($commandString);
         $command = \BDC_CaptchaHttpCommand::FromQuerystring($commandString);
         $responseBody = '';
         switch ($command) {
